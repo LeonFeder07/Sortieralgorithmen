29422b92b39e3c525b16a3f1bca8d2811f4bfd21
LeonFeder07 <leonfeder18@gmail.com>
2024-02-01 22:25:07 +0100

    selection klappt insertion ist eigentlich richtig klappt aber nicht

--- src/sortierer.java
+++ src/sortierer.java
@@ -7,20 +7,131 @@ public class sortierer {
     private JButton hinzufügenButton;
     private JButton sortierenButton;
     private JLabel wdawd;
+    private JPanel wda;
+    private JButton insertionSortierenButton;
+    List<Integer> sortiert;
+    List<Integer> unsortiert;
 
     public sortierer() {
-    hinzufügenButton.addActionListener(new ActionListener() {
-        @Override
-        public void actionPerformed(ActionEvent e) {
+        sortiert = new List<Integer>();
+        unsortiert = new List<Integer>();
+        hinzufügenButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                for (int i = 10; i >0; i--) {
+                    //unsortiert.append((int) (Math.random() * 10));
+                    unsortiert.append( i);
+                }
+                unsortiert.toFirst();
+                while (unsortiert.hasAccess()) {
+                    System.out.println(unsortiert.getContent());
+                    unsortiert.next();
+                }
+                System.out.println("-------einfügen ends--------\n");
+            }
+        });
+        sortierenButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                unsortiert.toFirst();
+                while (unsortiert.hasAccess()) {
+                    System.out.println(unsortiert.getContent());
+                    unsortiert.next();
+                }
+                System.out.println("------selection begins------\n");
+             //   if (!unsortiert.isEmpty()) {
+                    while (!unsortiert.isEmpty()) {
+                    unsortiert.toFirst();
+                    int lokalezeit = unsortiert.getContent();
 
-        }
-    });
-    sortierenButton.addActionListener(new ActionListener() {
-        @Override
-        public void actionPerformed(ActionEvent e) {
+                        while (unsortiert.hasAccess()) {
+                            unsortiert.next();
+                            if (unsortiert.hasAccess() && unsortiert.getContent() < lokalezeit) {
+                                lokalezeit = unsortiert.getContent();
 
-        }
-    });
-}
 
+                            }
+
+                        }
+                        sortiert.append(lokalezeit);
+                        unsortiert.toFirst();
+                        while (unsortiert.hasAccess()) {
+
+                            if (unsortiert.hasAccess() && unsortiert.getContent() == lokalezeit) {
+                                unsortiert.remove();
+                            }
+                            unsortiert.next();
+                        }
+
+                    }
+
+                sortiert.toFirst();
+                while (sortiert.hasAccess()) {
+                    System.out.println(sortiert.getContent());
+                    sortiert.next();
+                }
+                System.out.println("---------selection ends-------\n");
+            }
+        });
+        insertionSortierenButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                unsortiert.toFirst();
+                while (unsortiert.hasAccess()) {
+                    System.out.println(unsortiert.getContent());
+                    unsortiert.next();
+                }
+                System.out.println("-------insertion begins-------\n");
+                while (!unsortiert.isEmpty()) {
+                    boolean isteingefuegt=false;
+                    unsortiert.toFirst();
+                    if (unsortiert.hasAccess()) {
+                        int lokalezeit1 = unsortiert.getContent();
+                        unsortiert.remove();
+
+                        if(sortiert.isEmpty()){
+                            sortiert.append(lokalezeit1);
+                        }
+                        if (!sortiert.isEmpty()) {
+                            sortiert.toFirst();
+                            while (sortiert.hasAccess()) {
+                                if (sortiert.getContent() > lokalezeit1 && isteingefuegt==false) {
+                                    sortiert.insert(lokalezeit1);
+                                    isteingefuegt=true;
+
+                                }
+                                sortiert.next();
+
+                            }
+                            sortiert.append(lokalezeit1);
+
+                        }
+
+
+
+                    }
+                }
+                sortiert.toFirst();
+                while (sortiert.hasAccess()) {
+                    System.out.println(sortiert.getContent());
+                    sortiert.next();
+                }
+                System.out.println("-------insertion ends--------\n");
+                unsortiert.toFirst();
+                while (unsortiert.hasAccess()) {
+                    System.out.println(unsortiert.getContent());
+                    unsortiert.next();
+                }
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("sortierer");
+        frame.setContentPane(new sortierer().wda);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
